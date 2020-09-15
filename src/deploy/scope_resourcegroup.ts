b5ac3144eaabf9e497a0a69b5a6f9a58345f8c0f
kanika1894 <58769601+kanika1894@users.noreply.github.com>
2020-09-15 15:57:07 +0530

    Update scope_resourcegroup.ts

--- src/deploy/scope_resourcegroup.ts
+++ src/deploy/scope_resourcegroup.ts
@@ -3,7 +3,7 @@ import { exec } from '@actions/exec';
 import { ExecOptions } from '@actions/exec/lib/interfaces';
 import { ParseOutputs, Outputs } from '../utils/utils';
 
-export async function DeployResourceGroupScope(azPath: string, validationOnly: boolean, resourceGroupName: string, template: string, deploymentMode: string, deploymentName: string, parameters: string): Promise<Outputs> {
+export async function DeployResourceGroupScope(azPath: string, resourceGroupName: string, template: string, deploymentMode: string, deploymentName: string, parameters: string): Promise<Outputs> {
     // Check if resourceGroupName is set
     if (!resourceGroupName) {
         throw Error("ResourceGroup name must be set.")
@@ -17,13 +17,13 @@ export async function DeployResourceGroupScope(azPath: string, validationOnly: b
 
     // create the parameter list
     const azDeployParameters = [
-        resourceGroupName ? `--resource-group '${resourceGroupName}'` : undefined,
+        resourceGroupName ? `--resource-group ${resourceGroupName}` : undefined,
         template ?
-            template.startsWith("http") ? `--template-uri '${template}'` : `--template-file '${template}'`
+            template.startsWith("http") ? `--template-uri ${template}` : `--template-file ${template}`
             : undefined,
-        deploymentMode ? `--mode '${deploymentMode}'` : "Incremental",
-        deploymentName ? `--name '${deploymentName}'` : undefined,
-        parameters ? `--parameters '${parameters}'` : undefined
+        deploymentMode ? `--mode ${deploymentMode}` : "Incremental",
+        deploymentName ? `--name "${deploymentName}"` : undefined,
+        parameters ? `--parameters ${parameters}` : undefined
     ].filter(Boolean).join(' ');
 
     // configure exec to write the json output to a buffer
@@ -56,17 +56,19 @@ export async function DeployResourceGroupScope(azPath: string, validationOnly: b
     // validate the deployment
     core.info("Validating template...")
     var code = await exec(`"${azPath}" deployment group validate ${azDeployParameters} -o json`, [], validateOptions);
-    if (validationOnly && code != 0) {
+    if (deploymentMode.toLowerCase() === "validate" && code != 0) {
         throw new Error("Template validation failed.")
     } else if (code != 0) {
         core.warning("Template validation failed.")
     }
 
-    // execute the deployment
-    core.info("Creating deployment...")
-    var deploymentCode = await exec(`"${azPath}" deployment group create ${azDeployParameters} -o json`, [], deployOptions);
-    if (deploymentCode != 0) {
-        core.error("Deployment failed.")
+    if (deploymentMode.toLowerCase() != "validate") {
+        // execute the deployment
+        core.info("Creating deployment...")
+        var deploymentCode = await exec(`"${azPath}" deployment group create ${azDeployParameters} -o json`, [], deployOptions);
+        if (deploymentCode != 0) {
+            core.error("Deployment failed.")
+        }
     }
     core.debug(commandOutput);
 
